535f2c4ea465856df17ba99c1b8a70fe8e660430
Zin_User <as7227020@gmail.com>
2023-12-05 08:27:48 +0800

    GET

---
+++ my-app/src/global/constants.js
@@ -0,0 +1,2 @@
+export const API_HOST = "https://redteagame.ddns.me/WebServer"
+export const API_GET_DATA = `${API_HOST}/SS/1234`
--- my-app/src/pages/Home/index.js
+++ my-app/src/pages/Home/index.js
@@ -1,16 +1,38 @@
 import { useState, useEffect } from "react";
 
+import { API_GET_DATA } from '../../global/constants'
+
 import Edit from "./compoents/Edit";
 import MyList from "./compoents/List";
 import "./index.css";
 
+async function fetchDta(setData){
+//  const res = await  fetch(API_GET_DATA , {
+// method : "GET",
+// headers :{'Content-type' : 'application/json'}
+//  })
+const res = await fetch(API_GET_DATA)
+ const {data} = await res.json()
+console.log(data)
+setData(data)
+}
+
+
 const Home = () => {
   const [data, setData] = useState([]);
 
-  //data更動後就會觸發方法
+
+  //data更動後就會觸發方法 渲染畫面時也會執行一次
 useEffect(()=>{
 //data更動時要做的回應
-},[data])
+//window.alert("新增成功!");
+//綁定時
+fetchDta(setData)
+// return () =>{//取消綁定/清除時
+
+// }
+},[])
+
 
   return (
     <div className="app">
